25bfc257b88a57a340a9cf5cb26f4af7322d36bb
George Papagapitos <george.g.papagapitos@gmail.com>
2021-01-16 11:42:59 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let form = document.querySelector('form');
+let todoList = document.querySelector('ul');
+let button = document.querySelector('button');
+let input = document.getElementById('user-todo');
+
+let todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+
+localStorage.setItem('todos', JSON.stringify(todosArray));
+
+let storage = JSON.parse(localStorage.getItem('todos'));
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    todosArray.push(input.value);
+    localStorage.setItem('todos', JSON.stringify(todosArray));
+    todoMaker(input.value);
+    input.value = '';
+});
+
+let todoMaker = text => {
+    let todo = document.createElement("li");
+    todo.textContent = text;
+    todoList.appendChild(todo);
+}
+
+for(let i = 0; i < storage.length; i++) {
+    todoMaker(storage[i]);
+}
+
+button.addEventListener("click", function() {
+    localStorage.clear();
+    while(todoList.firstChild) {
+        todoList.removeChild(todoList.firstChild);
+    }
+});
+
